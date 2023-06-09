9f03834e3053196616645a4a805eddba93e522ce
chase <lowkvel@gmail.com>
2023-06-09 13:52:49 +0800

    vector init

---
+++ cpp/stl/containers/stl_vector.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printit(const vector<int> &v) {
+    for (auto i : v)
+        cout << i << " ";
+    cout << endl;
+}
+
+int main() {
+    // initialization
+    
+
+    return 0;
+}
